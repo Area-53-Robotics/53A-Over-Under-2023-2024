b7ac00840bb8cc1d1c1baa5a7c374cff1a8c7b45
enxinjie <140917719+enxinjie@users.noreply.github.com>
2024-02-08 20:08:42 -0500

    2/8/24
    Skills Auton

--- src/leftAuton.cpp
+++ src/leftAuton.cpp
@@ -17,10 +17,6 @@ void autonFromLSP () {
     moveBot(2, 1, 15, true);
     turnBot(60, 1, 50);
     moveBot(6, 1, 127, true);
-    /*
-    moveBot(5, 1, 100, false);
-    moveBot(30, 1, 127, true);
-    */
     moveBot(3, 1, 100, false);
     leftwings(true);
     moveBot(14, 1, 100, false);
@@ -28,15 +24,5 @@ void autonFromLSP () {
     turnBot(-70, 1, 50);
     moveBot(10, 1, 50, false);
     moveBot(15, 1, 50, false);
-
-
-    /*
-    swing(true, -15, 1, 35);
-    leftwings(true);
-    runIntake(true, -127);
-    moveBot(3, 1, 50, false);
-    leftwings(false);
-    turnBot(-60, 1, 50);
-    moveBot(18, 1, 60, false);
-    */
+    
 }
--- src/skillsAuton.cpp
+++ src/skillsAuton.cpp
@@ -5,6 +5,15 @@
 
 void skillsAuton () {
     
-    shoot(60000, 105);
+    blocker(false);
+    swing(true, -19.2, 1, 50);
+    shoot(10000, 105);
+    turnBot(40, 1, 50);
+    moveBot(40, 1, 127, true);
+    leftwings(true);
+    rightwings(true);
+    turnBot(-30, 1, 50);
+    moveBot(30, 1, 50, true);
+
 
 }
