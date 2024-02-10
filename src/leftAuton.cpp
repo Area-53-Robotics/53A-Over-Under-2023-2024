c4bbbfea143db2ae8236db42e22ff8edd9fc1a40
enxinjie <140917719+enxinjie@users.noreply.github.com>
2024-02-09 21:38:27 -0500

    2/9/24
    Left Auton (winPoint = true?)

--- src/leftAuton.cpp
+++ src/leftAuton.cpp
@@ -8,21 +8,20 @@ void autonFromLSP () {
     runIntake(true, -127);
     pros::delay(100);
     runIntake(true, 127);
-    moveBot(40, 1, 127, false);
+    moveBot(38, 1, 127, false);
     pros::delay(700);
     moveBot(32, 1, 127, true);
     turnBot(70, 1, 50);
     runIntake(true, -127);
     pros::delay(500);
     moveBot(2, 1, 15, true);
-    turnBot(60, 1, 50);
-    moveBot(6, 1, 127, true);
+    turnBot(55, 1, 50);
+    moveBot(3, 1, 127, true);
     moveBot(3, 1, 100, false);
     leftwings(true);
     moveBot(14, 1, 100, false);
     leftwings(false);
-    turnBot(-70, 1, 50);
-    moveBot(10, 1, 50, false);
-    moveBot(15, 1, 50, false);
+    turnBot(-45, 1, 50);
+    moveBot(17, 1, 50, false);
     
 }
